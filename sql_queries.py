62e01ad6c3e9d1e2ace051df5f5a7c04e70cf540
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-09-23 15:12:11 -0500

    UPDATED: Corrected syntax. ADD: DROP

--- sql_queries.py
+++ sql_queries.py
@@ -2,7 +2,7 @@
 
 songplay_table_drop = ""
 user_table_drop = ""
-song_table_drop = ""
+song_table_drop = "DROP TABLE IF EXISTS song_table;"
 artist_table_drop = ""
 time_table_drop = ""
 
@@ -14,7 +14,7 @@ songplay_table_create = ("""
 user_table_create = ("""
 """)
 
-song_table_create = (""" CREATE TABLE IF NOT EXISTS song_table (song_id, title, artist_id, year, duration);
+song_table_create = (""" CREATE TABLE IF NOT EXISTS song_table (song_id varchar, title varchar, artist_id varchar, year int, duration int);
 """)
 
 artist_table_create = ("""
@@ -49,5 +49,7 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [song_table_create]
+drop_table_queries = [song_table_drop]
+# create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+# drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
