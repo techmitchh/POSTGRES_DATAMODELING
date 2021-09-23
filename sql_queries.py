e6d4b99264c9c41572ddd3eb1a3a0ee62e9b3b32
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-09-23 13:05:58 -0500

    ADD: song table create, song table insert

--- sql_queries.py
+++ sql_queries.py
@@ -14,7 +14,7 @@ songplay_table_create = ("""
 user_table_create = ("""
 """)
 
-song_table_create = ("""
+song_table_create = (""" CREATE TABLE IF NOT EXISTS song_table (song_id, title, artist_id, year, duration);
 """)
 
 artist_table_create = ("""
@@ -31,7 +31,8 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
+song_table_insert = (""" INSERT into song_table (song_id, title, artist_id, year, duration)
+                         VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
