698bbffc2b3661411dd7a5b0b85abc6c2a345bce
Harvey-Mitchell <tech.mitchh@gmail.com>
2021-09-23 18:31:04 -0500

    ADD: Artist_Table ETL
    create/drop/insert Artist_Table
    Updated Query list to include Artist_Table

--- sql_queries.py
+++ sql_queries.py
@@ -3,7 +3,7 @@
 songplay_table_drop = ""
 user_table_drop = ""
 song_table_drop = "DROP TABLE IF EXISTS song_table;"
-artist_table_drop = ""
+artist_table_drop = "DROP TABLE IF EXISTS artist_table;"
 time_table_drop = ""
 
 # CREATE TABLES
@@ -17,7 +17,7 @@ user_table_create = ("""
 song_table_create = (""" CREATE TABLE IF NOT EXISTS song_table (song_id varchar, title varchar, artist_id varchar, year int, duration int);
 """)
 
-artist_table_create = ("""
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artist_table (artist_id varchar, artist_name varchar, artist_location varchar, artist_latitude float, artist_longitude float);
 """)
 
 time_table_create = ("""
@@ -35,7 +35,8 @@ song_table_insert = (""" INSERT into song_table (song_id, title, artist_id, year
                          VALUES (%s, %s, %s, %s, %s);
 """)
 
-artist_table_insert = ("""
+artist_table_insert = (""" INSERT into artist_table (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+                            VALUES (%s, %s, %s, %s, %s);
 """)
 
 
@@ -49,7 +50,7 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [song_table_create]
-drop_table_queries = [song_table_drop]
+create_table_queries = [song_table_create, artist_table_create]
+drop_table_queries = [song_table_drop, artist_table_drop]
 # create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 # drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
